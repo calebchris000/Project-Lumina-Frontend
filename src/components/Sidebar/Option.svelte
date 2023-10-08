8e33b7a84ddf2911127f4f672e2d76bae1f1737a
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-08 23:21:57 +0100

    Create dashboard

---
+++ src/components/Sidebar/Option.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let option;
+  export let payload;
+
+  const handleClick = () => {
+    dispatch("clicked", option);
+  };
+</script>
+
+{#if payload === option}
+  <button class="option" on:click={handleClick}>{option}</button>
+{:else}
+  <button class="option selected" on:click={handleClick}>{option}</button>
+{/if}
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;800&display=swap");
+  .option {
+    background-color: white;
+    color: #445569;
+    padding: 1rem;
+    display: flex;
+    border-radius: 10px;
+    box-shadow: none;
+    border: none;
+    width: 100%;
+    font-size: 1rem;
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    transition: all 0.3s ease;
+  }
+
+  .selected {
+    background-color: rgba(0, 0, 0, 0);
+    color: white;
+    transition: all 0.3s ease;
+
+  }
+</style>
---
+++ src/components/Sidebar/Sidebar.svelte
@@ -0,0 +1,45 @@
+<script>
+  import LogoSection from "./LogoSection.svelte";
+  import Option from "./Option.svelte";
+  import { selected_option } from "./sidebar_store";
+  let DashboardOptions = ["Dashboard", "Students", "Teachers", "Attendance", "Report Cards"];
+  let payload = 'Dashboard';
+  const handleSelected = (e) => {
+    const key = e.detail
+    payload = key
+    selected_option.set(payload)
+  };
+
+</script>
+
+<nav class="sidebar_container">
+  <LogoSection />
+  <div class="option_holder">
+    {#each DashboardOptions as option}
+      <Option {option} {payload} on:clicked={handleSelected} />
+    {/each}
+  </div>
+</nav>
+
+<style>
+  .sidebar_container {
+    background-color: #445569;
+    color: white;
+    width: 20%;
+    padding: 1rem;
+    font-family: sans-serif;
+    border-radius: 18px;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    margin: 2rem 0;
+  }
+
+  .option_holder {
+    margin: 3rem 0;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    user-select: none;
+  }
+</style>
