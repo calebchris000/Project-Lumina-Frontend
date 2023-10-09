af5f31006d1d46c2651a311c0e8e4b88412c1caa
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-09 11:30:34 +0100

    Add generic global card

---
+++ src/lib/common/generic_card.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<section class="card_generic">
+<slot></slot>
+</section>
+
+<style>
+
+.card_generic {
+    user-select: none;
+    background-color: white;
+    padding: 1.2rem 1rem;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+}
+</style>
