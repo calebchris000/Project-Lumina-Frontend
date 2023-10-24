9e6ec411f9027dde9477e2ac1c863f79eaed9fa6
Caleb Nwaizu <calebchris000@gmail.com>
2023-10-24 08:03:42 +0100

    Get all between years from the school inception to current Format code

--- src/routes/v1/students/[student_id]/GraphService/Attendance/Class/Weekly/AttendanceWeekly.svelte
+++ src/routes/v1/students/[student_id]/GraphService/Attendance/Class/Weekly/AttendanceWeekly.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import AttendanceWeekly from "./AttendanceWeekly";
   import { onMount } from "svelte";
+  import { store } from "src/store/store";
+  import SchoolToCurrentYear from "$lib/shared/SchoolToCurrentYear";
 
   onMount(async () => {
-    const AttendanceData = await AttendanceWeekly(2023);
+    let AttendanceData: {} = $store.graph.attendance.class.weekly;
+    await AttendanceWeekly(2023, 41);
     let validData: boolean = typeof AttendanceData === "object";
     // const emptyData: number[]
     await import("apexcharts").then((module) => {
@@ -26,7 +29,7 @@
           // },
         ],
         xaxis: {
-          categories: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri'],
+          categories: ["Mon", "Tue", "Wed", "Thur", "Fri"],
         },
         yaxis: {
           title: {
@@ -39,6 +42,22 @@
       chart.render();
     });
   });
+  const getAllYears = SchoolToCurrentYear(1990)
 </script>
+
+<section class="flex gap-2">
+  <div>
+    <!-- <label for="year">YEAR</label> -->
+    <select class="w-fit border border-[#ebebeb] bg-transparent p-1 m-0 font-poppins rounded-md text-sm" name="" id="">
+      {#each getAllYears as year (year) }
+        <option>{year}</option>
+      {/each}
+    </select>
+  </div>
+
+  <div>
+    <input value="41" class="w-16 border border-[#ebebeb] outline-none block text-sm rounded-md p-1" type="number" placeholder="text">
+  </div>
+</section>
 
 <div class="chart" id="chart" />
--- src/routes/v1/students/[student_id]/GraphService/Attendance/Class/Weekly/AttendanceWeekly.ts
+++ src/routes/v1/students/[student_id]/GraphService/Attendance/Class/Weekly/AttendanceWeekly.ts
@@ -2,22 +2,34 @@ import axios from "axios";
 import { env } from "$env/dynamic/public";
 import { page } from "$app/stores";
 import { getISOWeek } from "date-fns";
+import { store } from "src/store/store";
+
+let weeklyData: object;
+
+store.subscribe((value) => {
+  weeklyData = value.graph.attendance.class.monthly;
+});
+
 let student_id: string = "";
-let currentDate: Date = new Date()
-let weekNumber = getISOWeek(currentDate)
-const GetWeeklyAttendance = async (year: number = 2023, month: number = currentDate.getMonth() + 1, week_number: number = 41) => {
+let currentDate: Date = new Date();
+let weekNumber = getISOWeek(currentDate);
+const GetWeeklyAttendance = async (year: number = 2023, week_number: number = weekNumber) => {
   try {
     page.subscribe((value) => {
       student_id = value.params.student_id;
     });
 
-    const url = env.PUBLIC_API_URL;
-    const endpoint = `/api/v1/student-attendance/${student_id}/weekly?year=${year}&week_number=${week_number}`;
+    if (Array.isArray(weeklyData)) {
+      const url = env.PUBLIC_API_URL;
+      const endpoint = `/api/v1/student-attendance/${student_id}/weekly?year=${year}&week_number=${week_number}`;
 
-    const response = await axios.get(url + endpoint);
-    console.log(response.data);
-    const results = response.data;
-    return results;
+      const response = await axios.get(url + endpoint);
+      const results = response.data;
+      store.update((defaults) => {
+        defaults.graph.attendance.class.weekly = results;
+        return defaults;
+      });
+    }
   } catch (error) {
     return error;
   }
